a1350e46d382a40112932fe0c6cba16de3d27f39
codzk <zekemilne@gmail.com>
2023-11-30 19:49:49 +1000

    add division feature

---
+++ division.py
@@ -0,0 +1,16 @@
+# input numerator as n and denominator as d
+
+n = int(input("Enter numerator: "))
+d = int(input("Enter denominator: "))
+
+# if d = 0
+if d == 0:
+# display "denominator cannot be zero" as output"
+    print("denominator cannot be zero")
+
+# else 
+else:
+    # q = n/d
+    q = n/d
+    # display q
+    print(q)
